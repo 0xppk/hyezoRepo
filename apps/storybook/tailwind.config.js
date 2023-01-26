a8963d89d2fb2b6084aee36984add6c7d165f779
10004ok <hyezoprk@kakao.com>
2023-01-26 17:37:15 +0900

    🧹 chore: config updated

---
+++ apps/storybook/tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports = require("tailwind-config-hyezo/tailwind.config.js");
---
+++ packages/tailwind-config-hyezo/tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    // app content
+    `src/**/*.{js,ts,jsx,tsx}`,
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: ["class", '[data-mode="dark"]'],
+  theme: {
+    extend: {
+      colors: {
+        brandblue: colors.blue[500],
+        brandred: colors.red[500],
+      },
+    },
+  },
+  plugins: [],
+};

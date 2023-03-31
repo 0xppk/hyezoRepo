6e6a89068dc3254ee88ec805c620d8ebc9c154fd
hyezo <hyezoprk@kakao.com>
2023-03-31 19:13:17 +0900

    🐛 fix: build error fix

--- apps/keyboard/components/client/ChatSearchUsers.tsx
+++ apps/keyboard/components/client/ChatSearchUsers.tsx
@@ -4,22 +4,14 @@ import { Button } from "@hyezo/ui";
 import { useState } from "react";
 import { ChatSearchModal } from "~/components/client";
 import { ChatRoomList } from "~/components/server";
-import { useLoadAllUsers } from "~/hooks";
 
 export default function ChatSearchUsers() {
   const [isOpen, setIsOpen] = useState(false);
-  const { reloadAllUsers } = useLoadAllUsers();
 
   return (
     <>
       <ChatSearchModal isOpen={isOpen} setIsOpen={setIsOpen} />
-      <Button
-        onClick={() => {
-          reloadAllUsers();
-          setIsOpen(true);
-        }}
-        fullWidth
-      >
+      <Button onClick={() => setIsOpen(true)} fullWidth>
         Find User
       </Button>
       <ChatRoomList />

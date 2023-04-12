5d2890b57c9b33a2d862ede5c379ed532f2ccbeb
hyezo <hyezoprk@kakao.com>
2023-04-12 19:13:18 +0900

    🐛 fix(fix): fix

--- apps/keyboard/server/db.ts
+++ apps/keyboard/server/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../node_modules/.prisma/client";
+import { PrismaClient } from ".prisma/client";
 import { env } from "~/env.mjs";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };

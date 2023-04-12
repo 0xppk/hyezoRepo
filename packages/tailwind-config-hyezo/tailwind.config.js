cca6d80afa57806dac020e0109b9487bfe6464ba
hyezo <hyezoprk@kakao.com>
2023-04-12 21:31:43 +0900

    🐛 fix: remove line-clamp for it's included as default

--- packages/tailwind-config-hyezo/tailwind.config.js
+++ packages/tailwind-config-hyezo/tailwind.config.js
@@ -152,7 +152,6 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/line-clamp"),
     require("@tailwindcss/typography"),
 
     ({ addUtilities, matchUtilities }) => {

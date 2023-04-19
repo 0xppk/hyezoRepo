8d1697f6dd83f5777c0b181305591ef039f0059b
hyezo <hyezoprk@kakao.com>
2023-04-19 22:06:03 +0900

    ♻️ refactor: dependency modified

--- apps/keyboard/hooks/useUpdateNowSeeing.ts
+++ apps/keyboard/hooks/useUpdateNowSeeing.ts
@@ -1,10 +1,11 @@
 import { useCallback, useEffect } from "react";
+import { useEventListener } from "@hyezo/hooks";
 import { fetchPost } from "~/lib/utils";
 
 export default function useUpdateNowSeeing(chatRoomId: string) {
   const updateNowSeeing = useCallback(
     async (status: boolean) =>
-      await fetchPost<boolean>("/api/updateNowSeeing", {
+      await fetchPost("/api/updateNowSeeing", {
         body: JSON.stringify({ nowSeeing: status, chatRoomId }),
       }),
     [chatRoomId],
@@ -19,5 +20,10 @@ export default function useUpdateNowSeeing(chatRoomId: string) {
     return () => {
       clearNowSeeing();
     };
-  }, [chatRoomId]);
+  }, [chatRoomId, updateNowSeeing]);
+
+  // 페이지 이동하지 않고(= 언마운트x) 브라우저 종료할 경우
+  useEventListener("beforeunload", () => {
+    clearNowSeeing();
+  });
 }

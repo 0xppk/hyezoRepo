77b25828cdcf1248bdfac02ee035e919f70af1a0
hyezo <hyezoprk@kakao.com>
2023-04-21 17:49:32 +0900

    ✨ feat: latest message logic added

---
+++ apps/keyboard/pages/api/updateChatRoomLatestMessage.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerAuthSession } from "~/server/auth";
+import { prisma } from "~/server/db";
+
+type TData = { success: boolean };
+type TError = { message: string };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<TData | TError>,
+) {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method Not Allowed" });
+    return;
+  }
+
+  const session = await getServerAuthSession({ req, res });
+  if (!session?.user) {
+    res.status(401).json({ message: "You are not logined 🦠" });
+    return;
+  }
+
+  const { updateTime, chatRoomId }: { updateTime: string; chatRoomId: string } =
+    req.body;
+
+  try {
+    await prisma.chatRoom.update({
+      where: {
+        id: chatRoomId,
+      },
+      data: {
+        latestMessage: updateTime,
+      },
+    });
+
+    return res.status(202).json({ success: true });
+  } catch (error) {
+    return res.status(500).json({ message: (error as Error).message });
+  }
+}
---
+++ apps/keyboard/pages/api/updateLatestSeenMessage.ts
@@ -0,0 +1,55 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerAuthSession } from "~/server/auth";
+import { prisma } from "~/server/db";
+
+type TData = { success: boolean };
+type TError = { message: string };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<TData | TError>,
+) {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method Not Allowed" });
+    return;
+  }
+
+  const session = await getServerAuthSession({ req, res });
+  if (!session?.user) {
+    res.status(401).json({ message: "You are not logined 🦠" });
+    return;
+  }
+
+  const { updateTime, chatRoomId }: { updateTime: string; chatRoomId: string } =
+    req.body;
+
+  try {
+    const findParticipant = await prisma.chatParticipant.findFirstOrThrow({
+      where: {
+        chatRoomId,
+        userId: session.user.id,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const { id: myParticipantId } = findParticipant;
+
+    await prisma.chatParticipant.update({
+      where: {
+        id: myParticipantId,
+      },
+      data: {
+        latestSeenMessage: updateTime,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    return res.status(202).json({ success: true });
+  } catch (error) {
+    return res.status(500).json({ message: (error as Error).message });
+  }
+}

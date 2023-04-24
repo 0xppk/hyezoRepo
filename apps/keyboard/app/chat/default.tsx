1832d4aa86c5fed4b94407bb92a8a64c7935fc01
hyezo <hyezoprk@kakao.com>
2023-04-24 14:57:46 +0900

    💎 style(..): test for build

--- apps/keyboard/app/chat/default.tsx
+++ apps/keyboard/app/chat/default.tsx
@@ -1,27 +1,3 @@
-import { Metadata } from "next";
-import { ChatLayoutWrapper, ChatTabBarOnMobile } from "~/components/client";
-import { TabProvider } from "~/lib/contexts";
-
-export const metadata: Metadata = {
-  title: "Chat",
-  description: "a page for chatting to each users",
-};
-
-type ChatLayoutProps = {
-  userList: ReactNode;
-  recentInfo: ReactNode;
-} & LayoutProps;
-
-export default async function ChatLayout({ children }: ChatLayoutProps) {
-  return (
-    <TabProvider>
-      <ChatTabBarOnMobile />
-      <ChatLayoutWrapper>
-        <div className="min-w-full lg:col-span-2 lg:h-[60vh] lg:border-r lg:border-gray-900">
-          {children}
-        </div>
-        <div className="hidden lg:block lg:h-[30vh] lg:border-t lg:border-t-gray-900"></div>
-      </ChatLayoutWrapper>
-    </TabProvider>
-  );
+export default function DefaultPage() {
+  return <div>디폴트 페이지</div>;
 }

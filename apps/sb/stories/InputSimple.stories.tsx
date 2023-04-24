fef8a8c63bc2315f704e5293b50b3ed4ca66662c
hyezo <hyezoprk@kakao.com>
2023-04-24 10:43:27 +0900

    ✨ feat: input design test

--- apps/sb/stories/InputSimple.stories.tsx
+++ apps/sb/stories/InputSimple.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryFn } from "@storybook/react";
 import { InputSimple } from "@hyezo/ui";
+import { useState } from "react";
 
 export default {
   title: "UI/InputSimple",
@@ -17,11 +18,31 @@ export default {
   },
 } as Meta<typeof InputSimple>;
 
-const Template: StoryFn<typeof InputSimple> = args => <InputSimple {...args} />;
+type TExample = {
+  id: number;
+  name: string;
+}[];
+const example = [
+  { id: 0, name: "foo" },
+  { id: 1, name: "bar" },
+];
+const Template: StoryFn<typeof InputSimple> = args => {
+  const [data, setData] = useState<TExample>(example);
+
+  return (
+    <div className="flex items-center justify-center bg-black">
+      <InputSimple<{ id: number; name: string }>
+        {...args}
+        data={data}
+        setData={setData}
+      />
+    </div>
+  );
+};
 
 export const Sample = Template.bind({});
 Sample.args = {
-  submitAction: value => alert(value + " 〰️ 닉네임 생성 완료"),
+  labelKeys: ["foo"],
   placeholder: "닉네임을 입력하세요",
 };
 Sample.parameters = {

1e83fd79e6aae244d36e6f708e9fd62d4ecef3a7
hyezo <hyezoprk@kakao.com>
2023-04-26 03:25:08 +0900

    🐛 fix: layout fix

--- apps/keyboard/app/(market)/layout.tsx
+++ apps/keyboard/app/(market)/layout.tsx
@@ -1,3 +1,5 @@
 export default async function MarketLayout({ children }: LayoutProps) {
-  return <div className="overflow-y-auto p-5 sm:p-7 lg:col-span-3">{children}</div>;
+  return (
+    <div className="select-none overflow-y-auto p-5 sm:p-7 lg:col-span-3">{children}</div>
+  );
 }
--- apps/keyboard/app/chat/layout.tsx
+++ apps/keyboard/app/chat/layout.tsx
@@ -28,27 +28,25 @@ export default async function ChatLayout({
 
   return (
     <TabProvider>
-      <div className="overflow-hidden">
-        <ChatTabBarOnMobile />
-        <ChatLayoutWrapper>
-          <div className="min-w-full lg:col-span-2 lg:h-[60vh] lg:border-r lg:border-gray-900">
-            {children}
-          </div>
+      <ChatTabBarOnMobile />
+      <ChatLayoutWrapper>
+        <div className="min-w-full lg:col-span-2 lg:h-[60vh] lg:border-r lg:border-gray-900">
+          {children}
+        </div>
 
-          {/* replace @recentInfo for bug in deploy url */}
-          <div className="min-w-full overflow-auto p-7 lg:h-[60vh] lg:p-10">
-            <ChatSearchUserInput />
-            <ChatRoomList />
-          </div>
+        {/* replace @recentInfo for bug in deploy url */}
+        <div className="min-w-full overflow-auto p-7 lg:h-[60vh] lg:p-10">
+          <ChatSearchUserInput />
+          <ChatRoomList />
+        </div>
 
-          {/* replace @userList for bug in deploy url */}
-          <div className="min-w-full lg:col-span-2 lg:h-[30vh] lg:border-r lg:border-t lg:border-gray-900">
-            <ChatRecentInfo />
-          </div>
+        {/* replace @userList for bug in deploy url */}
+        <div className="min-w-full lg:col-span-2 lg:h-[30vh] lg:border-r lg:border-t lg:border-gray-900">
+          <ChatRecentInfo />
+        </div>
 
-          <div className="hidden lg:block lg:h-[30vh] lg:border-t lg:border-t-gray-900"></div>
-        </ChatLayoutWrapper>
-      </div>
+        <div className="hidden lg:block lg:h-[30vh] lg:border-t lg:border-t-gray-900"></div>
+      </ChatLayoutWrapper>
     </TabProvider>
   );
 }

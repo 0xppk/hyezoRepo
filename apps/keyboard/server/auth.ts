bdf5db8a4a55d29b7cfbf0ff05115235c878057f
hyezo <hyezoprk@kakao.com>
2023-04-25 18:19:25 +0900

    🐛 fix: to check user identification by prisma-id with token-id

--- apps/keyboard/server/auth.ts
+++ apps/keyboard/server/auth.ts
@@ -89,7 +89,7 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       const dbUser = await prisma.user.findFirst({
         where: {
-          email: token.email,
+          id: token.id,
         },
       });
 
